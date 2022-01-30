8222b28f0f96bfe6b8a82fd0f5264a774f58631d
nttminh-dev <mnguyen@bhcc.edu>
2022-01-29 23:18:39 -0500

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from './styles/Container.styled';
-import { Nav, StyledHeader } from './styles/Header.styled'
+import { Logo, Nav, StyledHeader } from './styles/Header.styled'
+import { Button } from './styles/Button.styled'
+
 
 function Header() {
     return <StyledHeader bg='red'>
         <Container>
             <Nav>
-                <Logo />
+                <Logo src='./images/logo.svg' alt='logo' />
                 <Button>Try it Free</Button>
             </Nav>
         </Container>
