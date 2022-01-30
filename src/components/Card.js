a0b4faf5ec3aaaab146fe8134be54e9b4d2c761e
nttminh-dev <mnguyen@bhcc.edu>
2022-01-29 23:55:57 -0500

    Added row-reverse for even number item

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledCard } from './styles/Card.styled'
 
 function Card({ item: { id, title, body, image } }) {
-    return <StyledCard>
+    return <StyledCard layout={id % 2 === 0 && 'row-reverse'}>
         <div>
             <h2>{title}</h2>
             <p>{body}</p>
