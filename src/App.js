6124684f19694f1c08cc31c9e5e093d217240b76
nttminh-dev <mnguyen@bhcc.edu>
2022-01-29 23:42:40 -0500

    Added Card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+import content from './content'
 
 const theme = {
   colors: {
@@ -9,6 +11,7 @@ const theme = {
     body: '#fff',
     footer: '#003333'
   },
+  mobile: '768px'
 }
 
 function App() {
@@ -18,7 +21,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => (
+            <Card item={item} key={index} />
+          ))}
         </Container>
       </>
     </ThemeProvider>
